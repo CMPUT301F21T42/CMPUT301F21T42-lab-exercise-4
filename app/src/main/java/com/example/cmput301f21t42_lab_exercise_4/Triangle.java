3e3997ac1350c180d5f10356051adbe5cbd10298
Raunak Agarwal <raunak1@ualberta.ca>
2021-09-30 18:59:32 -0600

    color added to Triangle class

--- app/src/main/java/com/example/cmput301f21t42_lab_exercise_4/Triangle.java
+++ app/src/main/java/com/example/cmput301f21t42_lab_exercise_4/Triangle.java
@@ -3,8 +3,11 @@ package com.example.cmput301f21t42_lab_exercise_4;
 public class Triangle extends Shape{
     public int x;
     public int y;
+    String color = "blue";
 
     public Triangle(int x, int y) {
         super(x, y);
     }
+
+
 }
