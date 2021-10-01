3131ae5b13fb5d16c3f9deb48c6679df81bafbd6
Wendy Zhang <qianyu1@ualberta.ca>
2021-09-30 19:03:24 -0600

    added color string

--- app/src/main/java/com/example/cmput301f21t42_lab_exercise_4/Shape.java
+++ app/src/main/java/com/example/cmput301f21t42_lab_exercise_4/Shape.java
@@ -3,10 +3,12 @@ package com.example.cmput301f21t42_lab_exercise_4;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color;
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+        this.color = "blue";
     }
     
 }
